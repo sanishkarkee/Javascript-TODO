d8bd2986d6a985f26a80176ae1e8743dd652be94
oscar <oscar@claimstrak.co.uk>
2023-10-10 13:49:31 +0545

    Js CRUD using localstorage concept

--- script.js
+++ script.js
@@ -32,10 +32,78 @@ function displayItems() {
         <button class='saveBtn'>Save</button>
         <button class='cancelBtn'>Cancel</button>
       </div>
-    </div>`
+    </div>
+    `
   }
 
   document.querySelector('.to-do-list').innerHTML = items
+
+  activateDeleteListners()
+  activateEditListners()
+  activateSaveListners()
+  activateCancelListners()
+}
+
+// -----------------------------------------------
+
+function activateDeleteListners() {
+  let deleteBtn = document.querySelectorAll('.deleteBtn')
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener('click', () => {
+      deleteItem(i)
+    })
+  })
+}
+
+//yo click garda save,cancel, text area visible hunu paryo
+function activateEditListners() {
+  const editBtn = document.querySelectorAll('.editBtn')
+  const updateController = document.querySelectorAll('.update-controller')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener('click', () => {
+      updateController[i].style.display = 'block'
+      inputs[i].disabled = false
+    })
+  })
+}
+
+function activateSaveListners() {
+  const saveBtn = document.querySelectorAll('.saveBtn')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  saveBtn.forEach((sb, i) => {
+    sb.addEventListener('click', () => {
+      updateItem(inputs[i].value, i)
+    })
+  })
+}
+
+function activateCancelListners() {
+  const cancelBtn = document.querySelectorAll('.cancelBtn')
+  const upadateController = document.querySelectorAll('.update-controller')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+
+  cancelBtn.forEach((cb, i) => {
+    cb.addEventListener('click', () => {
+      upadateController[i].style.display = 'none'
+      inputs[i].disabled = true
+    })
+  })
+}
+
+// -----------------------------------------------
+
+function updateItem(text, i) {
+  itemsArray[i] = text
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+function deleteItem(i) {
+  itemsArray.splice(i, 1)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
 }
 
 function displayDate() {
@@ -44,6 +112,8 @@ function displayDate() {
   document.querySelector('#date').innerHTML =
     date[1] + ' ' + date[2] + ' ' + date[3]
 }
+
+// -----------------------------------------------
 
 window.onload = function () {
   displayDate()
